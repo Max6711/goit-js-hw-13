3bba207df715d1be6d8e7466a39a00b5c2f5cbcd
Oleh Liubimov <mtbs1996@gmail.com>
2024-03-22 17:59:26 +0200

    main functional done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,25 +1,20 @@
 import axios from 'axios';
 
-async function fetchImg(searchInput) {
+async function fetchImg(searchWord, page = 1, limit = 15) {
   const apiKey = '42869495-3eaffa1d7f59c13a6a9af4ac7';
-  const searchWord = searchInput.value;
-  let page = 1;
-  let limit = 15;
-  const searchParams = new URLSearchParams({
-    page: page,
-    limit: limit,
-  });
-  const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchWord}&orientation=horizontal&safesearch=true&${searchParams}`;
 
-  const response = await axios.get(url);
-  return response.data;
+  const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchWord}&orientation=horizontal&safesearch=true&page=${page}&per_page=${limit}`;
 
-  //   return axios.get(url).then(response => {
-  //     if (!response.ok) {
-  //       throw new Error(response.status);
-  //     }
-  //     return response.json();
-  //   });
+  try {
+    if (!searchWord) {
+      throw new Error('Search word is empty');
+    }
+
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Failed to fetch images: ${error.message}`);
+  }
 }
 
 export default fetchImg;
--- src/main.js
+++ src/main.js
@@ -1,2 +1,63 @@
+const form = document.querySelector('.form');
+const loader = document.querySelector('.loader');
+const searchInput = document.querySelector('.search-input');
+let searchWord = '';
+const galWrap = document.querySelector('.gallery');
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+const moreBtn = document.querySelector('.more-btn');
+
+let page = 1;
+const limit = 15;
+
+import fetchImg from './js/pixabay-api';
+import createGalleryMarkup from './js/render-functions';
+const lightbox = new simpleLightbox('.gallery a', {});
+
+form.addEventListener('submit', async evt => {
+  galWrap.innerHTML = '';
+  evt.preventDefault();
+  try {
+    searchWord = searchInput.value;
+    const imagesData = await fetchImg(searchWord);
+    const { hits } = imagesData;
+
+    loader.classList.add('is-open');
+
+    if (hits.length === 0) {
+      iziToast.show({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        color: 'red',
+        position: 'topRight',
+      });
+    }
+
+    createGalleryMarkup(hits);
+    page += 1;
+    if (page > 1) {
+      moreBtn.classList.remove('hidden');
+    }
+    lightbox.refresh();
+    loader.classList.remove('is-open');
+    form.reset();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+moreBtn.addEventListener('click', async evt => {
+  evt.preventDefault();
+  try {
+    const imagesData = await fetchImg(searchWord, page, limit);
+    const { hits } = imagesData;
+    createGalleryMarkup(hits);
+    lightbox.refresh();
+  } catch (error) {
+    console.log(error);
+  }
+
+  form.reset();
+});
