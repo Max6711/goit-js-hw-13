3a03a12291a0c061e989b981bf58c5575ef5ad0b
Oleh Liubimov <mtbs1996@gmail.com>
2024-03-26 18:45:01 +0200

    2.0

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('.form');
 const loader = document.querySelector('.loader');
+const loaderSecond = document.querySelector('.loader-2');
 const searchInput = document.querySelector('.search-input');
 let searchWord = '';
 const galWrap = document.querySelector('.gallery');
@@ -9,8 +10,22 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 const moreBtn = document.querySelector('.more-btn');
 
+const showLoader = () => {
+  loader.classList.add('is-open');
+};
+const hideLoader = () => {
+  loader.classList.remove('is-open');
+};
+const showSecondLoader = () => {
+  loaderSecond.classList.remove('hidden')
+}
+const hideSecondLoader = () => {
+  loaderSecond.classList.add("hidden")
+}
+
 let page = 1;
-const limit = 15;
+const per_page = 15;
+const totalPages = Math.ceil(500 / per_page);
 
 import fetchImg from './js/pixabay-api';
 import createGalleryMarkup from './js/render-functions';
@@ -19,13 +34,18 @@ const lightbox = new simpleLightbox('.gallery a', {});
 form.addEventListener('submit', async evt => {
   galWrap.innerHTML = '';
   evt.preventDefault();
+  showLoader();
   try {
     searchWord = searchInput.value;
+    if (!searchWord) {
+      return iziToast.error({
+        position: 'topRight',
+        message: 'Search word is empty',
+      });
+    }
     const imagesData = await fetchImg(searchWord);
     const { hits } = imagesData;
-
-    loader.classList.add('is-open');
-
+    page += 1;
     if (hits.length === 0) {
       iziToast.show({
         message:
@@ -36,12 +56,11 @@ form.addEventListener('submit', async evt => {
     }
 
     createGalleryMarkup(hits);
-    page += 1;
-    if (page > 1) {
+    if (page >= 2) {
       moreBtn.classList.remove('hidden');
     }
     lightbox.refresh();
-    loader.classList.remove('is-open');
+    hideLoader();
     form.reset();
   } catch (error) {
     console.log(error);
@@ -50,8 +69,18 @@ form.addEventListener('submit', async evt => {
 
 moreBtn.addEventListener('click', async evt => {
   evt.preventDefault();
+  showSecondLoader()
+  if (page > totalPages) {
+    moreBtn.classList.add('hidden');
+    return iziToast.error({
+      position: 'topRight',
+      message: "We're sorry, but you've reached the end of search results.",
+    });
+  }
   try {
-    const imagesData = await fetchImg(searchWord, page, limit);
+    const imagesData = await fetchImg(searchWord, page, per_page);
+    hideSecondLoader();
+    page += 1;
     const { hits } = imagesData;
     createGalleryMarkup(hits);
     lightbox.refresh();
